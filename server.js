206817b75c01b4d2658229d3227bdb3f56f7215b
Yajaira Flynn-Corbin <yary.ba26@gmail.com>
2020-03-26 01:08:03 -0400

    changes

--- server.js
+++ server.js
@@ -12,14 +12,14 @@ app.use(morgan(morganSetting));
 app.use(helmet());
 app.use(cors());
 
-app.use(function validateBearerToken(req, res, next) {
-  const apiToken = process.env.API_TOKEN;
-  const authToken = req.get("Authorization");
-  if (!authToken || authToken.split(" ")[1] !== apiToken) {
-    return res.status(401).json({ error: "Unauthorized request" });
-  }
-  next();
-});
+// app.use(function validateBearerToken(req, res, next) {
+//   const apiToken = process.env.API_TOKEN;
+//   const authToken = req.get("Authorization");
+//   if (!authToken || authToken.split(" ")[1] !== apiToken) {
+//     return res.status(401).json({ error: "Unauthorized request" });
+//   }
+//   next();
+// });
 
 app.get("/movie", function handleGetMovie(req, res) {
   let response = MOVIES;
